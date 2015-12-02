d4211ad8c07bfcf1b197b7545c4c4828c3646e89
Paweł Pacana <pawel.pacana@gmail.com>
2015-12-02 19:52:03 +0100

    Get rid of superfluous actions for both sides.

--- write-model-side/app/controllers/orders_controller.rb
+++ write-model-side/app/controllers/orders_controller.rb
@@ -1,58 +1,32 @@
 class OrdersController < ApplicationController
   include CommandHandlers
 
-  # GET /orders
-  # GET /orders.json
-  def index
-    @orders = Order.all
-  end
-
-  # GET /orders/1
-  # GET /orders/1.json
-  def show
-    @order = Order.find(params[:id])
-    @order_lines = OrderLine.where(order_uid: @order.uid)
-  end
-
-  # GET /orders/new
-  def new
-    @order_id = SecureRandom.uuid
-    @products = Product.all
-    @customers = Customer.all
-  end
-
-  # POST /orders/:id/add_item
   def add_item
     cmd = Domain::Commands::AddItemToBasket.new(product_params)
     execute(cmd)
-
     head :ok
   end
 
-  # POST /orders/:id/remove_item
   def remove_item
     cmd = Domain::Commands::RemoveItemFromBasket.new(product_params)
     execute(cmd)
-
     head :ok
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     cmd = Domain::Commands::CreateOrder.new(order_params)
     execute(cmd)
-
     redirect_to Order.find_by_uid(cmd.order_id), notice: 'Order was successfully created.'
   end
 
   private
-    def product_params
-      args = params.permit(:id, :product_id)
-      {order_id: args[:id], product_id: args[:product_id]}
-    end
 
-    def order_params
-      params.permit(:order_id, :customer_id)
-    end
+  def product_params
+    args = params.permit(:id, :product_id)
+    {order_id: args[:id], product_id: args[:product_id]}
+  end
+
+  def order_params
+    params.permit(:order_id, :customer_id)
+  end
 end
